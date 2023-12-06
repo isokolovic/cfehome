9cf6790f93334bd5e49755f7994d230bf57fcafa
isokolovic <isokolovic1@gmail.com>
2023-12-06 11:33:06 +0100

    test

--- cfehome/proclog/models.py
+++ cfehome/proclog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import psutil
 
 # Create your models here.
+
+class Processes(models.Model):
+        process_list=models.CharField(max_length=2000)
+
+        process_list = "asdf"
+
+        def __str__(self):
+            return self.process_list
+
